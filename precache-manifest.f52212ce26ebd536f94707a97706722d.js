9c7ace0cbc2e20f6785b01c40057616fd7b14e43
ArieldRio <arieldelrioviamonte@gmail.com>
2021-06-04 15:44:00 -0500

    Updates

---
+++ precache-manifest.f52212ce26ebd536f94707a97706722d.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13d4c2750254fd9463da983f1188300d",
+    "url": "/recipe_app/index.html"
+  },
+  {
+    "revision": "9b33abbfa5b36f06ca13",
+    "url": "/recipe_app/static/css/2.faa62aa3.chunk.css"
+  },
+  {
+    "revision": "42a7e0e37e8bb1f85bb9",
+    "url": "/recipe_app/static/css/main.f09bd3e3.chunk.css"
+  },
+  {
+    "revision": "9b33abbfa5b36f06ca13",
+    "url": "/recipe_app/static/js/2.a94bc3e2.chunk.js"
+  },
+  {
+    "revision": "3ce03b8cad0cce25f379d427a7a96371",
+    "url": "/recipe_app/static/js/2.a94bc3e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "42a7e0e37e8bb1f85bb9",
+    "url": "/recipe_app/static/js/main.18608bdc.chunk.js"
+  },
+  {
+    "revision": "a9d402b641426411166e",
+    "url": "/recipe_app/static/js/runtime-main.0fbdace6.js"
+  },
+  {
+    "revision": "0aa8a9869323d181afb9a94c0007f888",
+    "url": "/recipe_app/static/media/english_language.0aa8a986.png"
+  },
+  {
+    "revision": "5962f2b6d24d310cb102e9803862a1b3",
+    "url": "/recipe_app/static/media/recipe.5962f2b6.svg"
+  },
+  {
+    "revision": "927ca3b125a7b73c7ffa95cf7a06d8e9",
+    "url": "/recipe_app/static/media/sad-face.927ca3b1.svg"
+  },
+  {
+    "revision": "7c74d8f0246f45d70d11e2e14e67aa4a",
+    "url": "/recipe_app/static/media/smile.7c74d8f0.svg"
+  },
+  {
+    "revision": "1b7fc0bf1d1b73a253f3fd7560dd207b",
+    "url": "/recipe_app/static/media/spanish_language.1b7fc0bf.png"
+  }
+]);
